b18c9e6d207b7dee31dd209391f2d3a5d193bdfd
foxane <65512187+foxane@users.noreply.github.com>
2024-04-24 15:09:00 +0700

    Added arithmethic object

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const btns = document.querySelectorAll('button');
 const inputArr = [];
 let result = 0;
 
+const calculate = {
+  sum: (operand1, opreand2) => operand1 + opreand2,
+  sub: (operand1, opreand2) => operand1 - opreand2,
+  mul: (operand1, opreand2) => operand1 * opreand2,
+  div: (operand1, opreand2) => operand1 / opreand2,
+};
+
+const containOperator = function (arr) {
+  const OPERATOR = ['+', '-', '*', '/'];
+  return arr.some((element) => OPERATOR.includes(element));
+};
+
 // Adding event handler for all btns
 btns.forEach((btn) =>
   btn.addEventListener('click', function () {
@@ -23,4 +35,41 @@ function inputHandler(btnNode) {
   }
 }
 
-function inputFilter(btn) {}
+function inputFilter(btn) {
+  // Dot
+  if (btn.classList.contains('dot')) {
+    // Input array is empty
+    if (inputArr.length === 0) {
+      inputArr.push(0);
+      inputArr.push('.');
+      // Input array already contain dot
+    } else if (inputArr.includes('.')) {
+      return;
+    } else {
+      inputArr.push('.');
+    }
+    // Number
+  } else if (btn.classList.contains('number')) {
+    // Input array is empty and number is 0
+    if (inputArr.length === 0 && btn.id === '0') {
+      return;
+    } else {
+      inputArr.push(btn.id);
+    }
+    // OPerator
+  } else {
+    if (!containOperator(inputArr) && inputArr.length > 0) {
+      operator = btn.textContent;
+    } else {
+      operate();
+    }
+  }
+}
+
+function operate() {
+  try {
+  } catch (error) {
+    display.value = 'FUCK YOU';
+    inputArr.length = 0;
+  }
+}
